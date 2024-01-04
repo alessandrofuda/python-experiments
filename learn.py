7c3b2c3d459b5284bb1e3c59f25f9bc897b83e0d
alessandrofuda <alessandro.fuda@gmail.com>
2024-01-04 17:53:59 +0100

    scripts/functions

--- learn.py
+++ learn.py
@@ -7,14 +7,14 @@ df.head()
 # x = df.iloc[:,:]
 # x = df.iloc[:,:-1].values  # array return
 # x = df.iloc[:,-1].values
-x = df.iloc[:,0].values
+x = df.iloc[:,:-1].values  # ':-1' IMPORTANT!
 # print(x)
 # print(x[0:5])
 
-y = df.iloc[:,1].values
+y = df.iloc[:,-1].values   # '-1' IMPORTANT!
 
 # visual representation of linear regression
-plt.scatter(x, y)
+# plt.scatter(x, y)
 # plt.show()
 
 
@@ -22,17 +22,28 @@ plt.scatter(x, y)
 # import only a function directly from a submodule of sklearn module (alternative syntax) - (only for memory optimization)
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-x_test.shape
-# print(x_train)
-# print(x_train.shape)
-# print(x_test)
-# print(x_test.shape)
-
+# x_train.shape
+# x_test.shape
+# y_train.shape
+# y_test.shape
 
 # Model in action: Build, Train, Predict, Evaluate
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(x_train, y_train)
-#print(model.predict([7]))
-# plt.scatter(x_test, y_test)
-# plt.plot()
+
+print('To predict values: 7, 3, 3.5 ')
+print('Predicted values: ', end=" ")
+print(model.predict([[7], [3], [3.5]]))
+
+print('--------------')
+
+print('x_test: ')
+print(x_test)
+y_pred = model.predict(x_test)
+print('y_pred (predicted): ', end="")
+print(y_pred)
+
+plt.scatter(x_test, y_test)
+plt.plot(x_test, y_pred, color='yellow')
+plt.show()
