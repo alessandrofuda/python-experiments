4becc54250ba2fa65f7ac7358334985d8870fd27
alessandrofuda <alessandro.fuda@gmail.com>
2024-02-21 17:14:27 +0100

    be chatbot complete

--- projects/chatbot_gpt_35/chatbot_chain.py
+++ projects/chatbot_gpt_35/chatbot_chain.py
@@ -6,6 +6,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.callbacks import get_openai_callback
+from langchain.memory import ConversationBufferMemory
 
 
 # setting env variables
@@ -13,49 +14,37 @@ load_dotenv()
 loader = CSVLoader(file_path="faq.csv")
 documents = loader.load()
 
-# print(documents)
-# exit()
-
 openai_api_key = os.getenv('OPENAI_API_KEY')
 embeddings_model = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
-# print(embeddings_model)
-# exit()
-
 # example of text to vectors conversion
-# embeddings = embeddings_model.embed_documents(
-#     [
-#         "Hi there!",
-#         "Oh, hello!"
-#     ]
-# )
-
-# print(embeddings)
-# exit()
+# embeddings = embeddings_model.embed_documents(["Hi there!", "Oh, hello!"])
 
 vectorstore = FAISS.from_documents(documents, embeddings_model)  # store documents/vectors in DB (FAISS)
-
-# print(vectorstore)
-# exit()
-
-# example of "similarity search" inside vector store (using text input)
-# results = vectorstore.similarity_search('Do you have a free plan?')
-# print(results)
-# exit()
-
-
+# results=vectorstore.similarity_search('Do you have free plan?') -> "similarity search" in vectorstore (with txt input)
 llm_model = ChatOpenAI(openai_api_key=openai_api_key)
-chain = ConversationalRetrievalChain.from_llm(llm=llm_model, retriever=vectorstore.as_retriever())  # 'recuperatore'
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+# -------------- MAIN -------------------------------------------------------------------------------
+chain = ConversationalRetrievalChain.from_llm(llm=llm_model,
+                                              retriever=vectorstore.as_retriever(),  # 'recuperatore'
+                                              memory=memory)
+# --------------------------------------------------------------------------------------------------
+
 
 # Return 3 most relevant documents
 # vectorstore.as_retriever(search_kwargs={"k": 3})
 
-query = "Do you have a team plan?"
-
 with get_openai_callback() as cb:
-    response = chain.invoke({"question": query, "chat_history": []})
+    # response = chain.invoke({"question": query, "chat_history": []})
+    query = "Do you have a team plan?"
+    response = chain.invoke({"question": query})
 
-print(response)
+    query2 = 'And what about free plan?'
+    response = chain.invoke({"question": query2})
+
+print(response['chat_history'])
+print('------------')
 print(cb)
 
 
