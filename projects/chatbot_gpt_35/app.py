a6f7c9da969e026d8a253e7ac4d2c2c35f4ae3fb
alessandrofuda <alessandro.fuda@gmail.com>
2024-03-04 17:17:51 +0100

    web vers chatbot

--- projects/chatbot_gpt_35/app.py
+++ projects/chatbot_gpt_35/app.py
@@ -1,37 +1,39 @@
-from flask import Flask  # , render_template, request, jsonify
-# from waitress import serve
-
-app = Flask(__name__)
+from chatbot_chain import get_chatbot_chain
+import streamlit as st  # is a web server too!
 
 
-@app.route('/')
-def homepage():
-    return "OOKK"
+chain = get_chatbot_chain()
+
+# URL for the logo of the assistant bot
+# We need it as a separate variable because it's used in multiple places
+bot_logo = 'https://pbs.twimg.com/profile_images/1739538983112048640/4NzIg1h6_400x400.jpg'
+
+# We use st.session_state and fill in the st.session_state.messages list
+# It's empty in the beginning, so we add the first message from the bot
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [{"role": "bot", "content": "Hello, how can I help?"}]
 
 
-# API
-# @app.route('/api/calculate/salary', methods=['POST'])
-# def salary_calculation_api():
-#     return jsonify({
-#         "salary": predict_salary(request.json['experience'], request.json['level'])
-#     })
-#
-#
-# # WEB form
-# @app.route('/calculate/salary', methods=['GET'])
-# def salary_calculation_get():
-#     # Jinja2 template engine is used to render html page
-#     return render_template('form.html')
-#
-#
-# @app.route('/calculate/salary', methods=['POST'])
-# def salary_calculation():
-#     experience = request.form['experience']
-#     level = request.form['level']
-#     salary = "{:.2f}".format(predict_salary(experience, level))
-#     return render_template('form.html', salary=salary, experience=experience, level=level)
+# Then we show all the chat messages in Markdown format
+for message in st.session_state['messages']:
+    if message["role"] == 'bot':
+        with st.chat_message(message["role"], avatar=bot_logo):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
+# We ask for the user's question, append it to the messages and show below
+if query := st.chat_input("Please ask your question here:"):
+    st.session_state.messages.append({"role": "user", "content": query})
+    with st.chat_message("user"):
+        st.markdown(query)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-#     serve(app, host="0.0.0.0", port=8080)
+    # We create a new chat message and launch the "chain" for the answer
+    with st.chat_message("assistant", avatar=bot_logo):
+        message_placeholder = st.empty()
+        result = chain.invoke({"question": query})
+        message_placeholder.markdown(result['answer'])
+
+    # We also add the answer to the messages history
+    st.session_state.messages.append({"role": "bot", "content": result['answer']})
